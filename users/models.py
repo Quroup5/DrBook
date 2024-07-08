fe9e513ca53c4bd259b6ee749bef3ca45d53aae2
Armin Shabani <batman1990ar@gmail.com>
2024-07-08 19:34:45 +0330

    Implement: models for patient|Refactore:core\Settings.py have been changed and is ready to use PostgreSQL,Generated a sercret key and added to the .env file

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Patients(models.Model):
+    unique_id = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
+    national_id = models.CharField(max_length=10, unique=True,
+                                   validators=[RegexValidator(r'^\d{10}$', 'Enter a valid 10-digit national ID.')])
+    address = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=14, decimal_places=2, default=0)
+
+
