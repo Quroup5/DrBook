533a24192c5f99bf833fdb1f89746e4537ba8b7f
eucalyptian <saeid.badamchizadeh@gmail.com>
2024-07-09 19:31:05 +0330

    add transaction model

--- settings/urls.py
+++ settings/urls.py
@@ -3,14 +3,10 @@ from .views import (
     TransactionListView,
     TransactionDetailView,
     TransactionCreateView,
-    TransactionUpdateView,
-    TransactionDeleteView,
 )
 app_name = "settings"
 urlpatterns = [
     path('transactions/', TransactionListView.as_view(), name='transaction_list'),
     path('transactions/<int:pk>/', TransactionDetailView.as_view(), name='transaction_detail'),
     path('transactions/new/', TransactionCreateView.as_view(), name='transaction_create'),
-    path('transactions/<int:pk>/edit/', TransactionUpdateView.as_view(), name='transaction_update'),
-    path('transactions/<int:pk>/delete/', TransactionDeleteView.as_view(), name='transaction_delete'),
 ]
