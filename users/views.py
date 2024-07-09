937d999e841c44501cd1a98b53ea17212603d70b
aminmoghadasi <amin.moghadasi84@gmail.com>
2024-07-10 00:32:24 +0330

    user model creation

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from .forms import UserRegisterForm
 
 
 # Create your views here.
-def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+class UserRegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'register.html'
+
+    def get(self, requset, *args, **kwargs):
+        form = UserRegisterForm()
+        return render(request, 'register.html', {'form': form})
