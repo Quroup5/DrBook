559a50ad8882b199acf8a9b3af54a8519f1b99e1
eucalyptian <saeid.badamchizadeh@gmail.com>
2024-07-09 16:36:55 +0330

    updated views.py and urls.py

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import path
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Transaction
+
+class TransactionListView(ListView):
+    model = Transaction
+    template_name = 'transactions/transaction_list.html'
+    context_object_name = 'transactions'
+
+
+class TransactionDetailView(DetailView):
+    model = Transaction
+    template_name = 'transactions/transaction_detail.html'
+    context_object_name = 'transaction'
+
+
+class TransactionCreateView(CreateView):
+    model = Transaction
+    template_name = 'transactions/transaction_form.html'
+    fields = ['patient', 'price']
+    success_url = reverse_lazy('transaction_list')
--- core/urls.py
+++ core/urls.py
@@ -20,3 +20,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.urls import path
+from .views import (
+    TransactionListView,
+    TransactionDetailView,
+    TransactionCreateView,
+    TransactionUpdateView,
+    TransactionDeleteView,
+)
+
+urlpatterns = [
+    path('transactions/', TransactionListView.as_view(), name='transaction_list'),
+    path('transactions/<int:pk>/', TransactionDetailView.as_view(), name='transaction_detail'),
+    path('transactions/new/', TransactionCreateView.as_view(), name='transaction_create'),
+    path('transactions/<int:pk>/edit/', TransactionUpdateView.as_view(), name='transaction_update'),
+    path('transactions/<int:pk>/delete/', TransactionDeleteView.as_view(), name='transaction_delete'),
+]
