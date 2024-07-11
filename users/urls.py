3f5509c3b39b72248f0a01c9adb357da26c9f4e1
aminmoghadasi <amin.moghadasi84@gmail.com>
2024-07-11 16:20:44 +0330

    user model creation

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('register/', views.UserRegisterView.as_view(), name='register'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
+    path('home/', views.home, name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 
 
 # Create your views here.
 class UserRegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'register.html'
+    success_url = reverse_lazy('login')
 
-    def get(self, requset, *args, **kwargs):
-        form = UserRegisterForm()
-        return render(request, 'register.html', {'form': form})
+
+class UserLoginView(CreateView):
+    form_class = UserLoginForm
+    template_name = 'login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        if request.method == 'POST':
+            form = UserLoginForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+
+                user = authenticate(username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, f"you are logged in as {username}")
+                    return redirect('home')
+                else:
+                    messages.error(requset, "Error")
+            else:
+                messages.error(request, "Username or password incorrect")
+        form = UserLoginForm()
+        return render(request, 'login.html', {"form": form})
+
+
+def home(requset):
+    return render(requset, 'home.html')
